6928dc7366415e93daf1b83cbe6a2b41fd18c357
Bruno da Silva <brunocarreiro@gmail.com>
2020-04-19 16:22:39 -0700

    Updating the back with what I asked in week 2 assignment

--- sample_backend.py
+++ sample_backend.py
@@ -1,7 +1,14 @@
 from flask import Flask
 from flask import request
+from flask import jsonify
+from flask_cors import CORS
+import random
+import string
 
 app = Flask(__name__)
+
+#CORS stands for Cross Origin Requests.
+CORS(app) #Here we'll allow requests coming from any domain. Not recommended for production environment.
 
 users = { 
     'users_list' :
@@ -38,23 +45,55 @@ users = {
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/users')
+@app.route('/users', methods=['GET', 'POST'])
 def get_users():
-    search_username = request.args.get('name')
-    if search_username :
-        subdict = {'users_list' : []}
-        for user in users['users_list']:
-            if user['name'] == search_username:
-                subdict['users_list'].append(user)
-        return subdict
-    return users
+    if request.method == 'GET':
+        search_username = request.args.get('name')
+        search_job = request.args.get('job')
+        if search_username and search_job :
+            return find_users_by_name_job(search_username, search_job)  
+        elif search_username :
+            return find_users_by_name(search_username)  
+        return users
+    elif request.method == 'POST':
+        userToAdd = request.get_json()
+        userToAdd['id'] = generate_id()
+        users['users_list'].append(userToAdd)
+        resp = jsonify(userToAdd), 201
+        #resp.status_code = 200 #optionally, you can always set a response. 
+        # 200 is the default code for a normal response
+        return resp
 
-@app.route('/users/<id>')
+@app.route('/users/<id>', methods=['GET', 'DELETE'])
 def get_user(id):
     if id :
         for user in users['users_list']:
             if user['id'] == id:
-                return user
-        return ({})
+                if request.method == 'GET':
+                    return user
+                elif request.method == 'DELETE':
+                    users['users_list'].remove(user)
+                    resp = jsonify({}), 204
+                    return resp
+        resp = jsonify({"error": "User not found"}), 404
+        return resp
     return users
+
+def find_users_by_name(name):
+    subdict = {'users_list' : []}
+    for user in users['users_list']:
+        if user['name'] == name:
+            subdict['users_list'].append(user)
+    return subdict   
+
+def find_users_by_name_job(name, job):
+    subdict = {'users_list' : []}
+    for user in users['users_list']:
+        if user['name'] == name and user['job'] == job:
+            subdict['users_list'].append(user)
+    return subdict  
+
+def generate_id():
+    lettersAndDigits = string.ascii_letters + string.digits
+    return ''.join((random.choice(lettersAndDigits) for i in range(6)))
     
